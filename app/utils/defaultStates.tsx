7901f8fa243e2e790bcffead95fcf87b05191b0c
arsh <135307874+arsh342@users.noreply.github.com>
2024-11-12 00:42:08 +0530

    some changes

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Delhi",
+    country: "IN",
+    state: "Delhi",
+    lat: 28.6139,
+    lon: 77.2090
+  },
+  {
+    name: "London",
+    country: "GB",
+    state: "England",
+    lat: 51.5074,
+    lon: 0.1278,
+  },
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "Sydney",
+    country: "AU",
+    state: "New South Wales",
+    lat: -33.8688197,
+    lon: 151.2092955,
+  },
+  {
+    name: "Tokyo",
+    country: "JP",
+    state: "Tokyo",
+    lat: 35.6895,
+    lon: 139.6917
+  },
+];
+
+export default defaultStates;
